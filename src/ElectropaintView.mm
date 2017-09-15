2b4da533b2bb6695b21441eb4e183dad20c5975d
Ricardo Banffy <rbanffy@gmail.com>
2017-09-15 14:27:57 +0100

    Fix wrong assumption

--- src/ElectropaintView.mm
+++ src/ElectropaintView.mm
@@ -46,8 +46,6 @@
 #include <algorithm>
 #include <functional>
 
-#define NUM_THINGIES 120
-
 
 struct color_type {
     CGFloat red;
@@ -299,7 +297,7 @@ random_generator_type z_delta_change(0.4, 0.7, 200, false, 0.005, 0.0005);
                       color_type(red_movement(),
                                  green_movement(),
                                  blue_movement()));
-    wings.resize(NUM_THINGIES, newwing);
+    wings.resize(40, newwing);
     
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 }
