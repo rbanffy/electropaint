d66ea21420410ed28b3f9601cac533b3db0b155c
Ricardo Banffy <rbanffy@gmail.com>
2017-09-15 14:00:13 +0100

    Reformat

--- src/ElectropaintView.mm
+++ src/ElectropaintView.mm
@@ -11,23 +11,23 @@
  * http://www.lloydslounge.org/electropaintosx/
  *
  * Created by Douglas McInnes on 12/17/04.
- * Copyright (c) 2004, Kent RosenKoetter, Douglas McInnes. 
+ * Copyright (c) 2004, Kent RosenKoetter, Douglas McInnes.
  * All rights reserved.
  *
  * ported from Kent Rosenkoetter's electropaint.cpp:
  * http://legolas.homelinux.org/~kent/electropaint/
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- */ 
+ */
 
 #import "ElectropaintView.h"
 
@@ -49,44 +49,44 @@
 
 
 struct color_type {
-	CGFloat red;
-	CGFloat green;
-	CGFloat blue;
-
-	color_type(void) : red(1), green(1), blue(1)
-	{ };
-	color_type(CGFloat r, CGFloat g, CGFloat b) : red(r), green(g), blue(b)
-	{ };
-	color_type(const color_type & c) : red(c.red), green(c.green), blue(c.blue)
-	{ };
+    CGFloat red;
+    CGFloat green;
+    CGFloat blue;
+    
+    color_type(void) : red(1), green(1), blue(1)
+    { };
+    color_type(CGFloat r, CGFloat g, CGFloat b) : red(r), green(g), blue(b)
+    { };
+    color_type(const color_type & c) : red(c.red), green(c.green), blue(c.blue)
+    { };
 };
 
 
 struct wing_type
 {
-	CGFloat radius;		// all angles in degrees, per OpenGL
-	CGFloat angle;
-	CGFloat delta_angle;
-	CGFloat z_delta;
-	CGFloat roll;
-	CGFloat pitch;
-	CGFloat yaw;
-	color_type color;
-	color_type edge_color;
-	CGFloat alpha;
-
-	wing_type(void) : radius(10), angle(0), delta_angle(15), z_delta(0.5),
-		roll(0), pitch(0), yaw(0),
-		color(), edge_color(), alpha(1)
-	{ }
-	wing_type(CGFloat _rad, CGFloat _ang, CGFloat _dang, CGFloat _dz,
-		CGFloat _roll, CGFloat _pitch, CGFloat _yaw,
-		const color_type & _c, const color_type & _ec = color_type(),
-		CGFloat _a = 1) : radius(_rad), angle(_ang), delta_angle(_dang), z_delta(_dz),
-			roll(_roll), pitch(_pitch), yaw(_yaw),
-			color(_c), edge_color(_ec),
-			alpha(_a)
-	{ }
+    CGFloat radius;		// all angles in degrees, per OpenGL
+    CGFloat angle;
+    CGFloat delta_angle;
+    CGFloat z_delta;
+    CGFloat roll;
+    CGFloat pitch;
+    CGFloat yaw;
+    color_type color;
+    color_type edge_color;
+    CGFloat alpha;
+    
+    wing_type(void) : radius(10), angle(0), delta_angle(15), z_delta(0.5),
+    roll(0), pitch(0), yaw(0),
+    color(), edge_color(), alpha(1)
+    { }
+    wing_type(CGFloat _rad, CGFloat _ang, CGFloat _dang, CGFloat _dz,
+              CGFloat _roll, CGFloat _pitch, CGFloat _yaw,
+              const color_type & _c, const color_type & _ec = color_type(),
+              CGFloat _a = 1) : radius(_rad), angle(_ang), delta_angle(_dang), z_delta(_dz),
+    roll(_roll), pitch(_pitch), yaw(_yaw),
+    color(_c), edge_color(_ec),
+    alpha(_a)
+    { }
 };
 
 
@@ -174,209 +174,209 @@ random_generator_type z_delta_change(0.4, 0.7, 200, false, 0.005, 0.0005);
 /*****************************/
 
 - (id)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview {
-  self = [super initWithFrame:frame isPreview:isPreview];
-  if (self) {
-    [self setAnimationTimeInterval:1/60.0];
-
-    NSOpenGLPixelFormatAttribute attr[] = { 
-	  NSOpenGLPFANoRecovery,
-      NSOpenGLPFAAccelerated,
-	  NSOpenGLPFAColorSize, (NSOpenGLPixelFormatAttribute) 24,
-	  NSOpenGLPFAAlphaSize, (NSOpenGLPixelFormatAttribute) 8,
-	  NSOpenGLPFAStencilSize, (NSOpenGLPixelFormatAttribute) 0,
-	  NSOpenGLPFAWindow,
-	  (NSOpenGLPixelFormatAttribute) 0,
-    };
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/60.0];
+        
+        NSOpenGLPixelFormatAttribute attr[] = {
+            NSOpenGLPFANoRecovery,
+            NSOpenGLPFAAccelerated,
+            NSOpenGLPFAColorSize, (NSOpenGLPixelFormatAttribute) 24,
+            NSOpenGLPFAAlphaSize, (NSOpenGLPixelFormatAttribute) 8,
+            NSOpenGLPFAStencilSize, (NSOpenGLPixelFormatAttribute) 0,
+            NSOpenGLPFAWindow,
+            (NSOpenGLPixelFormatAttribute) 0,
+        };
+        
+        NSOpenGLPixelFormat *format = [[[NSOpenGLPixelFormat alloc] initWithAttributes:attr] autorelease];
+        
+        glview = [[[NSOpenGLView alloc] initWithFrame:NSZeroRect pixelFormat:format] autorelease];
+        
+        [self initGL];
+    }
     
-    NSOpenGLPixelFormat *format = [[[NSOpenGLPixelFormat alloc] initWithAttributes:attr] autorelease];
-      
-    glview = [[[NSOpenGLView alloc] initWithFrame:NSZeroRect pixelFormat:format] autorelease];
-
-    [self initGL];
-  }
-
-  return self;
+    return self;
 }
 
 - (void)startAnimation {
-  if (![glview isDescendantOf:self]) {
-    [self addSubview:glview];
-  }
-  [super startAnimation];
+    if (![glview isDescendantOf:self]) {
+        [self addSubview:glview];
+    }
+    [super startAnimation];
 }
 
 - (void)stopAnimation {
-  [super stopAnimation];
+    [super stopAnimation];
 }
 
 - (void)drawRect:(NSRect)rect {
-  [super drawRect:rect];
+    [super drawRect:rect];
 }
 
 - (void)animateOneFrame {
-	// animate
-	wings.pop_back();
-	wings.push_front(wing_type(radius_change(),
-			angle_change(),
-			delta_angle_change(),
-			z_delta_change(),
-			roll_change(),
-			pitch_change(),
-			yaw_change(),
-			color_type(red_movement(),
-				green_movement(),
-				blue_movement())));
-
-	//glutPostRedisplay();
-  [self display];
-
-  return;
+    // animate
+    wings.pop_back();
+    wings.push_front(wing_type(radius_change(),
+                               angle_change(),
+                               delta_angle_change(),
+                               z_delta_change(),
+                               roll_change(),
+                               pitch_change(),
+                               yaw_change(),
+                               color_type(red_movement(),
+                                          green_movement(),
+                                          blue_movement())));
+    
+    //glutPostRedisplay();
+    [self display];
+    
+    return;
 }
 
 - (BOOL)hasConfigureSheet {
-  return NO;
+    return NO;
 }
 
 - (NSWindow*)configureSheet {
-  return nil;
+    return nil;
 }
 
 - (void)setFrameSize:(NSSize)newSize {
-  [glview setFrameSize:newSize];
-  [self reshape:newSize];
-  [super setFrameSize:newSize];
+    [glview setFrameSize:newSize];
+    [self reshape:newSize];
+    [super setFrameSize:newSize];
 }
 
 - (void)dealloc {
-  [glview removeFromSuperview];
-  [super dealloc];
+    [glview removeFromSuperview];
+    [super dealloc];
 }
 
 
 - (void)initGL {
-	srand((int)time(NULL));
-
-  [[glview openGLContext] makeCurrentContext];
-  GLint params[] = { 1 };
-  CGLSetParameter(CGLGetCurrentContext(),  kCGLCPSwapInterval, params);
-
-	glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE);
-
-	//glutDisplayFunc(display);
-	//glutReshapeFunc(reshape);
-
-	glEnable (GL_LINE_SMOOTH);
-	glHint (GL_LINE_SMOOTH_HINT, GL_DONT_CARE);
-	glBlendFunc (GL_SRC_ALPHA, GL_ONE);
-
-	glEnable(GL_DEPTH_TEST);
-	glDepthMask(GL_FALSE);
-		
+    srand((int)time(NULL));
+    
+    [[glview openGLContext] makeCurrentContext];
+    GLint params[] = { 1 };
+    CGLSetParameter(CGLGetCurrentContext(),  kCGLCPSwapInterval, params);
+    
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE);
+    
+    //glutDisplayFunc(display);
+    //glutReshapeFunc(reshape);
+    
+    glEnable (GL_LINE_SMOOTH);
+    glHint (GL_LINE_SMOOTH_HINT, GL_DONT_CARE);
+    glBlendFunc (GL_SRC_ALPHA, GL_ONE);
+    
+    glEnable(GL_DEPTH_TEST);
+    glDepthMask(GL_FALSE);
+    
 #if defined(GL_VERSION_1_1)
-  glPolygonOffset(-0.5, -2);
+    glPolygonOffset(-0.5, -2);
 #endif
-
-	wing_dl = glGenLists(1);
-	glNewList(wing_dl, GL_COMPILE);
-	glBegin(GL_QUADS);
-	// default z-value is 0
-	// default normal is (0, 0, 1);
-	glVertex2f(1, 1);
-	glVertex2f(-1, 1);
-	glVertex2f(-1, -1);
-	glVertex2f(1, -1);
-	glEnd();
-	glEndList();
-
-	wing_type newwing(radius_change(),
-			angle_change(),
-			delta_angle_change(),
-			z_delta_change(),
-			roll_change(),
-			pitch_change(),
-			yaw_change(),
-			color_type(red_movement(),
-				green_movement(),
-				blue_movement()));
-	wings.resize(40, newwing);
-
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    
+    wing_dl = glGenLists(1);
+    glNewList(wing_dl, GL_COMPILE);
+    glBegin(GL_QUADS);
+    // default z-value is 0
+    // default normal is (0, 0, 1);
+    glVertex2f(1, 1);
+    glVertex2f(-1, 1);
+    glVertex2f(-1, -1);
+    glVertex2f(1, -1);
+    glEnd();
+    glEndList();
+    
+    wing_type newwing(radius_change(),
+                      angle_change(),
+                      delta_angle_change(),
+                      z_delta_change(),
+                      roll_change(),
+                      pitch_change(),
+                      yaw_change(),
+                      color_type(red_movement(),
+                                 green_movement(),
+                                 blue_movement()));
+    wings.resize(40, newwing);
+    
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 }
 
 - (void)display {
-
-  [[glview openGLContext] makeCurrentContext];
-
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glLoadIdentity();
-	gluLookAt(0, 50, 50,
-		0, 0, 13,
-		0, 0, 1);
-
-	std::list<wing_type>::const_iterator i(wings.begin());
-	std::list<wing_type>::const_iterator end(wings.end());
-	unsigned int count(0);
-	//if (hasOpenGL(1, 1)) {
+    
+    [[glview openGLContext] makeCurrentContext];
+    
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();
+    gluLookAt(0, 50, 50,
+              0, 0, 13,
+              0, 0, 1);
+    
+    std::list<wing_type>::const_iterator i(wings.begin());
+    std::list<wing_type>::const_iterator end(wings.end());
+    unsigned int count(0);
+    //if (hasOpenGL(1, 1)) {
 #if defined(GL_VERSION_1_1)
-	//	if (hasOpenGL(1, 1))
-			glEnable(GL_POLYGON_OFFSET_LINE);
+    //	if (hasOpenGL(1, 1))
+    glEnable(GL_POLYGON_OFFSET_LINE);
 #endif
-		glPushMatrix();
-		while (i != end) {
-			wing_type wing(*i++);
-
-			glTranslatef(0, 0, wing.z_delta);
-			glPushMatrix();
-			glRotatef(wing.angle + count * wing.delta_angle,
-				0, 0, 1);
-			glTranslatef(wing.radius, 0, 0);
-			glRotatef(-wing.yaw, 0, 0, 1);
-			glRotatef(-wing.pitch, 0, 1, 0);
-			glRotatef(wing.roll, 1, 0, 0);
-
-			glDisable(GL_BLEND);
-		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-		glColor3f(wing.color.red, wing.color.green, wing.color.blue);
-		glCallList(wing_dl);
-
-			glEnable (GL_BLEND);
-			glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-			glColor3f(wing.edge_color.red, wing.edge_color.green, wing.edge_color.blue);
-			glCallList(wing_dl);
-
-		glPopMatrix();
-
-		count++;
-	}
-		glPopMatrix();
-	glFlush();
-	//checkGLErrors(std::cerr);
-	glutSwapBuffers();
+    glPushMatrix();
+    while (i != end) {
+        wing_type wing(*i++);
+        
+        glTranslatef(0, 0, wing.z_delta);
+        glPushMatrix();
+        glRotatef(wing.angle + count * wing.delta_angle,
+                  0, 0, 1);
+        glTranslatef(wing.radius, 0, 0);
+        glRotatef(-wing.yaw, 0, 0, 1);
+        glRotatef(-wing.pitch, 0, 1, 0);
+        glRotatef(wing.roll, 1, 0, 0);
+        
+        glDisable(GL_BLEND);
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+        glColor3f(wing.color.red, wing.color.green, wing.color.blue);
+        glCallList(wing_dl);
+        
+        glEnable (GL_BLEND);
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+        glColor3f(wing.edge_color.red, wing.edge_color.green, wing.edge_color.blue);
+        glCallList(wing_dl);
+        
+        glPopMatrix();
+        
+        count++;
+    }
+    glPopMatrix();
+    glFlush();
+    //checkGLErrors(std::cerr);
+    glutSwapBuffers();
 }
 
 
 - (void)reshape:(NSSize)size {
-  [[glview openGLContext] makeCurrentContext];
-	glViewport(0, 0, (int)size.width, (int)size.height);
-
-	CGFloat xmult(1.0);
-	CGFloat ymult(1.0);
-//	float aspect(width / static_cast<float>(height));
-	if (size.width > size.height) {
-		xmult = size.width / size.height;
-	} else {
-		ymult = size.height / size.width;
-	}
-
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	glOrtho(-20 * xmult, 20 * xmult,
-		-20 * ymult, 20 * ymult,
-		35, 105);
-	glMatrixMode(GL_MODELVIEW);
-
-	//checkGLErrors(std::cerr);
-  //[[glview openGLContext] update];
+    [[glview openGLContext] makeCurrentContext];
+    glViewport(0, 0, (int)size.width, (int)size.height);
+    
+    CGFloat xmult(1.0);
+    CGFloat ymult(1.0);
+    //	float aspect(width / static_cast<float>(height));
+    if (size.width > size.height) {
+        xmult = size.width / size.height;
+    } else {
+        ymult = size.height / size.width;
+    }
+    
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-20 * xmult, 20 * xmult,
+            -20 * ymult, 20 * ymult,
+            35, 105);
+    glMatrixMode(GL_MODELVIEW);
+    
+    //checkGLErrors(std::cerr);
+    //[[glview openGLContext] update];
 }
 
 @end
